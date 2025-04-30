5bb25da6f0cd03dbe0ee1a3f51cf9faa1ae70ce9
Seulg2027 <seulgi2027@dgu.ac.kr>
2025-04-30 20:22:04 +0900

    feat : vpc, subnet, alb 구성 중

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+    default = "ap-northeast-2" # 서울
+}
+
+variable "azs" {
+    type    = list(string)
+    default = ["ap-northeast-2a", "ap-northeast-2c"]
+}
+
+variable "cidr_block" {
+    type = String
+}
