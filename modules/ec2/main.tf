303d610a4b9e5c5a8616ec117ca2254bdf1853b6
ssoyeonni <thduswja23@gmail.com>
2025-05-09 17:37:48 +0900

    feat : ALB를 위한 public subnet 추가 생성

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,40 +4,10 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id[count.index]
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  vpc_security_group_ids = [var.app_sg]
 
   tags = {
     Name = "${var.name_prefix}-${count.index}"
   }
 }
 
-resource "aws_security_group" "app_sg" {
-  name        = "app_sg"
-  description = "Allow HTTP traffic to ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [var.alb_sg_id]
-  }
-
-  ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [var.alb_sg_id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "app_sg"
-  }
-}
