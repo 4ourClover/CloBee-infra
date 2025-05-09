aa56da560ba8b9db24b93d9f3e70c1cbf327f7f2
Seulg2027 <seulgi2027@dgu.ac.kr>
2025-05-09 11:25:33 +0900

    feat: architecture 수정 - public subnet 하나 빼기, alb 하나

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,4 +1,4 @@
-# Subnet 1
+# public
 resource "aws_subnet" "public-subnet1" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.10.0/24"
@@ -9,6 +9,7 @@ resource "aws_subnet" "public-subnet1" {
     }
 }
 
+# Subnet 1
 resource "aws_subnet" "private-subnet1" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.20.0/24"
@@ -20,17 +21,7 @@ resource "aws_subnet" "private-subnet1" {
 }
 
 # Subnet 2
-resource "aws_subnet" "public-subnet2" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = "10.0.11.0/24"
-    availability_zone       = var.azs[1]
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "${var.vpc_name}-public-subnet-2"
-    }
-}
-
-resource "aws_subnet" "public-subnet2" {
+resource "aws_subnet" "private-subnet2" {
     vpc_id                  = aws_vpc.main.id
     cidr_block              = "10.0.21.0/24"
     availability_zone       = var.azs[1]
