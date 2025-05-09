484cd7841bb9d71242eeae39a22bce560ce53f9a
Seulg2027 <seulgi2027@dgu.ac.kr>
2025-05-09 11:50:02 +0900

    fix : alb - modules variables 추가

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
 module "vpc" {
-  source     = "./modules/vpc"
-  vpc_name   = "clobee-vpc"
-  cidr_block = var.cidr_block
+    source     = "./modules/vpc"
+    vpc_name   = "clobee-vpc"
+    cidr_block = var.cidr_block
 }
 
 module "ec2" {
-  source = "./modules/ec2"
-  vpc_id = module.vpc.vpc_id
-  azs    = var.azs
+    source     = "./modules/ec2"
+    vpc_id     = module.vpc.vpc_id
+    vpc_name   = var.vpc_name
+    azs        = var.azs
 }
 
 module "alb" {
-  source   = "./modules/alb"
-  alb_name = "my-public-alb"
-  subnets = [
-    module.subnet.public_subnet1_id,
-    module.subnet.public_subnet2_id
-  ]
+    source     = "./modules/alb"
+    alb_name   = "my-public-alb"
+    vpc_name   = var.vpc_name
+    subnets           = [
+        module.ec2.public_subnet1_id,
+        module.ec2.public_subnet2_id
+    ]
 }
